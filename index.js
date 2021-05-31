084e66ad92a9b4fdfbc4ffd12ba937a36d746083
김현홍 <hhkim26@douzone.com>
2021-05-31 09:51:05 +0900

    이벤트

--- index.js
+++ index.js
@@ -63,6 +63,8 @@ class Start {
     }
 }
 
-const start = new Start();
+document.addEventListener('DOMContentLoaded', () => {
+    const start = new Start();
 
-start.initialize();
+    start.initialize();
+})
